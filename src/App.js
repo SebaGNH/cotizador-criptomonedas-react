6d144ed118eaff0ed5c5a47861275fb951cd96c6
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-09-13 17:58:52 -0300

    se agregaron estilos con Styled-components en App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import styled from 'styled-components';
+import imagen from "./img/cryptomonedas.png";
+
 
 function App() {
   return (
     <>
-
+      <DivContenedor>
+        <div className="img">
+          <Imagen src={imagen} alt="Imagen Cripto" />
+        </div>
+        <div>
+          <H1>Cotiza Criptomonedas al instante</H1>
+        </div>
+      </DivContenedor>
     </>
   );
 }
+
+const DivContenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); // 50% u 50%
+    column-gap: 2rem;
+  }
+  `;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
+
 
 export default App;
